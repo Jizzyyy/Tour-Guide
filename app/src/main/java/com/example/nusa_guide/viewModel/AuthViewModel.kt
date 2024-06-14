4babe434de27754eb926f0aa0ca06e3e8d735898
Oppie Ramadhanti <oppieramadhanti@gmail.com>
2024-06-14 14:51:12 +0700

    feat: integration auth from API - remove unused field on register screen because not requested by API

--- app/src/main/java/com/example/nusa_guide/viewModel/AuthViewModel.kt
+++ app/src/main/java/com/example/nusa_guide/viewModel/AuthViewModel.kt
@@ -1,5 +1,34 @@
 package com.example.nusa_guide.viewModel
 
-class AuthViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.nusa_guide.model.LoginModel
+import com.example.nusa_guide.model.RegisterModel
+import com.example.nusa_guide.repository.AuthRepository
+import kotlinx.coroutines.launch
+
+class AuthViewModel : ViewModel() {
+
+
+    private val authRepository = AuthRepository()
+
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            val loginModel = LoginModel(email, password);
+
+            val response =authRepository.login(loginModel);
+
+        }
+    }
+
+
+    fun register(email: String, username: String, password: String ) {
+        viewModelScope.launch {
+            val registerModel = RegisterModel(email, username, password)
+
+            val response = authRepository.register(registerModel)
+        }
+    }
 
 }
